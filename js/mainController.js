85ab49a22a16e2af7e7c94cee2deb2238ad2d07d
Ifat BR <ifat.3d@gmail.com>
2021-02-10 14:01:40 +0200

    add another line, get indx of clicked line if any

--- js/mainController.js
+++ js/mainController.js
@@ -1,60 +1,144 @@
 'use strict';
 var gElCanvas;
 var gCtx;
+var gCurrLineIdx = 0;
+const gTouchEvs = ['touchstart', 'touchmove', 'touchend'];
 
 function init() {
     initImageGallery();
-    gElCanvas = document.getElementById('main-canvas')
-    gCtx = gElCanvas.getContext('2d')
-    drawImgFromlocal()
+    gElCanvas = document.getElementById('main-canvas');
+    gCtx = gElCanvas.getContext('2d');
+    addListeners();
+    drawImgFromlocal();
     //TODO:  resizeCanvas()
     //TODO: add event listener
-
 }
 
-
-function initImageGallery(){
+function initImageGallery() {
     createImageGallery();
-    
 }
 
-function onChooseImg(id){
+function onChooseImg(id) {
     updateMemeImage(id);
     drawImgFromlocal();
-
 }
 
-function onEditMemeText(elTextInput){
-    const txt= elTextInput.value;
-    updateMemeTxt(0,txt);
+function onEditMemeText(elTextInput, lnIdx) {
+    const txt = elTextInput.value;
+    updateMemeTxt(lnIdx, txt);
+    drawImgFromlocal();
+}
+
+function onChangeFontSize(sizeDiff) {
+    changeFontSize(gCurrLineIdx, sizeDiff);
     drawImgFromlocal();
 }
 
 function drawImgFromlocal() {
-    const img = new Image()
+    const img = new Image();
     img.src = getImgSrc();
     img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height) //img,x,y,xend,yend
-    const txt = getImgTxt();
-    addMemeText(txt,0,40)
-        
-    }
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height); //img,x,y,xend,yend
+        const txt1 = getLnObjectById(0).txt;
+        const txt2 = getLnObjectById(1).txt;
+
+        addMemeText(0, txt1, 200, 40);
+        addMemeText(1, txt2, 200, 360);
+    };
 }
 
-function addMemeText(text, x,y){
-    gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'red'
-    gCtx.fillStyle = 'white'
-    gCtx.font = '40px impact'
-    gCtx.textAlign = 'left'
-    gCtx.fillText(text, x, y)
-    gCtx.strokeText(text, x, y)
+function addMemeText(lnIdx, text, x, y) {
+    const lnObj = getLnObjectById(lnIdx);
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = 'red';
+    gCtx.fillStyle = 'white';
+    const fontSize = lnObj.size + 'px';
+    gCtx.font = fontSize + ' impact';
+    gCtx.textAlign = lnObj.align;
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+    updateTxtWidth(lnIdx, gCtx.measureText(text).width);
 }
-
 
 //For when I want to add download
 function downloadCanvas(elLink) {
-    const data = gElCanvas.toDataURL()
-    elLink.href = data
-    elLink.download = 'my-img.jpg'
+    const data = gElCanvas.toDataURL();
+    elLink.href = data;
+    elLink.download = 'my-img.jpg';
+}
+
+
+//LISTENERS
+function addListeners() {
+    addMouseListeners();
+    addTouchListeners();
+    window.addEventListener('resize', () => {
+        resizeCanvas();
+        renderCanvas();
+    });
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousemove', onMove);
+
+    gElCanvas.addEventListener('mousedown', onDown);
+
+    gElCanvas.addEventListener('mouseup', onUp);
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchmove', onMove);
+
+    gElCanvas.addEventListener('touchstart', onDown);
+
+    gElCanvas.addEventListener('touchend', onUp);
+}
+
+function onDown(ev) {
+    const pos = getEvPos(ev);
+    if (getClickedLine(pos)) console.log('line clicked!');
+    // if (!isCirlceClicked(pos)) return
+    // gCircle.isDragging = true
+    // gStartPos = pos
+    document.body.style.cursor = 'grabbing';
+}
+
+function onMove(ev) {
+    // if (gCircle.isDragging) {
+    //     const pos = getEvPos(ev)
+    //     const dx = pos.x - gStartPos.x
+    //     const dy = pos.y - gStartPos.y
+    //     gCircle.pos.x += dx
+    //     gCircle.pos.y += dy
+    //     gStartPos = pos
+    //     renderCanvas()
+    //     renderCircle()
+    // }
+}
+
+function onUp() {
+    // gCircle.isDragging = false
+    // document.body.style.cursor = 'grab'
+}
+
+function getEvPos(ev) {
+    var pos = {
+        x: ev.offsetX,
+        y: ev.offsetY,
+    };
+    if (gTouchEvs.includes(ev.type)) {
+        ev.preventDefault();
+        ev = ev.changedTouches[0];
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+        };
+    }
+    return pos;
+}
+
+function isLineClicked(clickedPos) {
+    // const { pos } = gCircle
+    // const distance = Math.sqrt((pos.x - clickedPos.x) ** 2 + (pos.y - clickedPos.y) ** 2)
+    // return distance <= gCircle.size
 }
--- js/memeService.js
+++ js/memeService.js
@@ -7,19 +7,31 @@ var gMeme = {
     selectedLineIdx: 0,
     lines: [
         {
-            txt: 'I can do this!',
-            size: '20',
-            align: 'left',
+            txt: '',
+            txtWidth:0,
+            size: 20,
+            align: 'center',
             color: 'blue',
+            x:200,
+            y:40
+        },
+        {
+            txt: '',
+            txtWidth:0,
+            size: 20,
+            align: 'center',
+            color: 'blue',
+            x:200,
+            y:360
         },
     ],
 };
+
 
 function createImageGallery(){
     for(var i=1; i<=18; i++){
         gImgs.push({id:i, url:`img/${i}.jpg`})
     }
-    console.log('gImgs:', gImgs)
     return gImgs;
 }
 
@@ -32,17 +44,46 @@ function getImgSrc(){
     return gImgs[idx].url;
 }
 
-function getImgTxt(){
+function getLnObjectById(lnIdx){
     const idx = getImgIdxById();
-    return gMeme.lines[0].txt;
-    //Todo: add line idx
+    return gMeme.lines[lnIdx];
 }
+
+function changeFontSize(lnIdx, sizeDiff){
+    if(sizeDiff <0 && gMeme.lines[lnIdx].size < 20 || sizeDiff > 0 && gMeme.lines[lnIdx].size > 80) return;
+    gMeme.lines[lnIdx].size += sizeDiff;
+}
+
 
 function updateMemeTxt(lnIdx,txt){
     gMeme.lines[lnIdx].txt = txt;
 }
 
+function updateTxtWidth(lnIdx, txtWidth){
+    gMeme.lines[lnIdx].txtWidth = txtWidth;
+}
 
 function getImgIdxById(){
     return gImgs.findIndex((img) => gMeme.selectedImgId === img.id);
+}
+
+function logMeme(){
+    console.log('gMeme:', gMeme)
+}
+
+function getClickedLine(clickedPos) {
+    console.log('clicke pos y',clickedPos.y)
+    const idx = gMeme.lines.findIndex((ln) => {
+        return (ln.x-(ln.txtWidth/2)) < clickedPos.x &&
+        (ln.x+(ln.txtWidth/2)) > clickedPos.x &&
+        (ln.y-(ln.size/2)) < clickedPos.y &&
+        (ln.y+(ln.size/2)) > clickedPos.y
+    });
+    console.log('idx:',idx);
+
+    if(!idx) return;
+    return gMeme.lines[idx];
+    // const { pos } = gCircle
+    // const distance = Math.sqrt((pos.x - clickedPos.x) ** 2 + (pos.y - clickedPos.y) ** 2)
+    // return distance <= gCircle.size
 }
